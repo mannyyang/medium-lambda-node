0d3fcac9585c6dde4e4139c28b252b1ba51c663b
Manuel Yang <myang@csod.com>
2017-08-22 21:18:46 -0700

    removed json safety string

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const axios = require("axios"); // Include the axios lib - run npm install axios
-exports.handler = function(event, context) {
+exports.handler = (event, context, callback) => {
     // TODO: pass username as an argument
     const username = 'mannyyang';
-    return axios.get(`https://medium.com/@${username}/latest?format=json`);
+    return axios.get(`https://medium.com/@${username}/latest?format=json`)
+        .then(response => {
+            callback(null, JSON.parse(response.data.replace('])}while(1);</x>', '')));
+        })
+        .catch(err => {
+            callback(err);
+        });
 };
